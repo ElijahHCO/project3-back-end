64685a4574f34dbb1ee56e961a3f16e508e09d51
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-03-30 12:13:07 -0600

    css

--- models/equipment.js
+++ models/equipment.js
@@ -6,7 +6,8 @@ const equipmentSchema = new Schema({
     type: {type:String, required: true},
     productBrand: {type:String, unique: true},
     productModel: {type:String, required: true},
-    quantity: {type:Number, default: 0, required: true}
+    quantity: {type:Number, default: 0, required: true},
+    rented: {type:Number, default: 0,}
 }, {timestamps: true})
 
 const Equipment = mongoose.model('Equipment', equipmentSchema);
