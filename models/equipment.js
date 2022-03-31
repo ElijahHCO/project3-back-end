2e5a146550a16a2c4e609be1ec4929bf83ff7383
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-03-31 14:08:53 -0600

    ski route

--- models/equipment.js
+++ models/equipment.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 //User model
 const equipmentSchema = new Schema({
     type: {type:String, required: true},
-    productBrand: {type:String, unique: true},
-    productModel: {type:String, required: true},
+    productBrand: {type:String, required: true},
+    productModel: {type:String, unique: true, required: true},
     quantity: {type:Number, default: 0, required: true},
     rented: {type:Number, default: 0,}
 }, {timestamps: true})
